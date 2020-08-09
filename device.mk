2218a5a9764a68fd03ed02f7490cdedfa344ca3a
Lucchetto <lucchetto.tie@live.com>
2020-08-09 09:17:13 +0200

    zippo: drop CAF power hal
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I47f597932fd546185e904f1e355443ea6a3630b0

--- device.mk
+++ device.mk
@@ -301,10 +301,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/perf/perfboostsconfig.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
