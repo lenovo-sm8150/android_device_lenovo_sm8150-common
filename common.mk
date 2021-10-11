cb52526d96757e5420100b63ab6f88eba2fb78c5
Lucchetto <lucchetto.tie@live.com>
2021-10-11 21:37:56 +0200

    sm8150-common: drop QTI perf
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I3be77b29bc2069abebc10dbddd84929b09ca50b4

--- common.mk
+++ common.mk
@@ -313,17 +313,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/perf/perfboostsconfig.xml
-
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
