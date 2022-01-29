3276025d85095912eb72d820e64977ae5c0cd4e7
Lucchetto <lucchetto.tie@live.com>
2022-01-29 13:46:34 +0100

    [WIP] sm8150-common: ignore neverallows
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,6 +170,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
