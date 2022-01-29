piscesmk2013/device_oneplus_sm8150-common

sm8150-common: use SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
BOARD_PLAT_PRIVATE_SEPOLICY_DIR and BOARD_PLAT_PUBLIC_SEPOLICY_DIR
has been deprecated.

Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

BoardConfigCommon.mk
@@ -158,8 +158,8 @@ VENDOR_SECURITY_PATCH := 2021-12-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
 # Sensors

lenovo-sm8150/android_device_lenovo_sm8150-common

BoardConfigCommon.mk
@@ -167,8 +167,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH_COMMON)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH_COMMON)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/vendor
 
 # Verified Boot